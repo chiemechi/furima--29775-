c23a89f2d9f3247af9108abfad88a3601f4a4a15
chiemechi <70624406+chiemechi@users.noreply.github.com>
2020-09-25 10:41:27 +0900

    エラー文表示

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,24 +5,24 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
          
-         validates :email, uniqueness: true,format: { with: /@/, message: '全角文字を使用してください' } 
+         validates :email, uniqueness: true,format: { with: /@/ } 
 
          validates  :password,presence: true, length: { minimum:6}
          
          PASSWORD_REGEX  = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
-         validates_format_of :password, with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください' 
+         validates_format_of :password, with: PASSWORD_REGEX
          
 
 
          validates  :birth_day, presence: true
         
 
-         with_options presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: '全角文字を使用してください' } do
+         with_options presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/} do
           validates :first_name
           validates :last_name
         end
 
-        with_options presence: true, format: {with:/\A[ア-ン゛゜ァ-ォャ-ョー「」、]+\z/,message: '全角カタカナを使用してください'}do
+        with_options presence: true, format: {with:/\A[ア-ン゛゜ァ-ォャ-ョー「」、]+\z/}do
           validates :first_name_kana
           validates :last_name_kana
         end
