17e3db082c53d4052723498a00acef993f929620
chiemechi <70624406+chiemechi@users.noreply.github.com>
2020-09-27 19:45:21 +0900

    アクティブハッシュの実装

---
+++ app/models/item.rb
@@ -0,0 +1,17 @@
+class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :category
+  belongs_to_active_hash :area
+  belongs_to_active_hash :charge
+  belongs_to_active_hash :condition
+  belongs_to_active_hash :send_date
+  belongs_to :user
+
+
+  with_options presence: true, numericality: { other_than: 1 } do
+  validates :category
+  validates :area
+  validates :charge
+  validates :condition
+  validates :send_date
+  end
--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,21 @@
 
 ActiveRecord::Schema.define(version: 2020_09_22_145437) do
 
+  create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name", null: false
+    t.text "description", null: false
+    t.integer "price", null: false
+    t.integer "category_id", null: false
+    t.integer "condition_id", null: false
+    t.integer "charges_id", null: false
+    t.integer "area_id", null: false
+    t.integer "date_id", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_items_on_user_id"
+  end
+
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "nickname", null: false
     t.string "last_name", null: false
@@ -30,4 +45,5 @@ ActiveRecord::Schema.define(version: 2020_09_22_145437) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "items", "users"
 end
