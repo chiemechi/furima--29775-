f1909c63e310ebea8006fc5fffd25779c769a7e8
chiemechi <70624406+chiemechi@users.noreply.github.com>
2020-09-27 19:47:18 +0900

    テーブル作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_22_145437) do
+ActiveRecord::Schema.define(version: 2020_09_27_104034) do
 
   create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -20,7 +20,7 @@ ActiveRecord::Schema.define(version: 2020_09_22_145437) do
     t.integer "condition_id", null: false
     t.integer "charges_id", null: false
     t.integer "area_id", null: false
-    t.integer "date_id", null: false
+    t.integer "send_id", null: false
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
