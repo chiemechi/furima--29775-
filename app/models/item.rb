9f4c13d5795cc4dfd9c215f05f7ff695a5f8bc6c
chiemechi <70624406+chiemechi@users.noreply.github.com>
2020-09-29 20:50:15 +0900

    itemテーブルテストコード

--- app/models/item.rb
+++ app/models/item.rb
@@ -13,7 +13,7 @@ class Item < ApplicationRecord
   
   validates  :description, presence: true
   validates  :name, presence: true
-  validates  :price, presence: true,numericality: {greater_than_or_equal_to: 300,less_than: 9999999}
+  validates  :price, presence: true,numericality: {greater_than_or_equal_to: 300,less_than: 9999999},format: {with: /\A[0-9]+\z/}
   validates  :image, presence: true
 
   with_options presence: true, numericality: { other_than: 1 } do
--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,20 @@
 FactoryBot.define do
   factory :item do
-    
+    name            { 'ああ' }
+    description     { 'あああああああ' }
+    price              { 1111 }
+    category_id { 2 }
+    condition_id            { 2 }
+    charges_id              { 2 }
+    area_id       { 2 }
+    send_date_id       { 2 }
+    association :user
+
+    after(:build) do |item|
+      item.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
+    end
+ 
   end
 end
+
+
