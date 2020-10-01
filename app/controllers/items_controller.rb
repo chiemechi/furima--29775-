c69a43693dc089175b7438d83e0bac993ce65765
chiemechi <70624406+chiemechi@users.noreply.github.com>
2020-10-01 12:07:29 +0900

    Revert "コントローラーの設定"
    This reverts commit b7f94573746a84804551c682bd238285c34d38df.

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :move_to_index, except: [:index, :show]
-  before_action :set_items, only: [:show, :edit, :update]
+  before_action :set_items, only: :show
 
 
   def index
@@ -22,18 +22,6 @@ class ItemsController < ApplicationController
   end
 
   def show
-  end
-
-  def edit
-
-  end
-
-  def update
-    if@item.update(item_params)
-      redirect_to root_path
-    else
-      render :edit
-    end
   end
 
   private
